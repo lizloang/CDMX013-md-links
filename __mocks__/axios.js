b5ce9faff1f52f88dc2bd8787a0873a555c80eb8
lizloang <liz.loang@gmail.com>
2022-10-31 14:03:53 -0600

    delete some comments

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,9 +1,22 @@
 const axios = {
   get: jest.fn((link) => {
-    if (link === "https://www.google.com/") {
+    if (
+      link === "https://www.google.com/" ||
+      link === "https://es.wikipedia.org/wiki/Markdown"
+    ) {
       return Promise.resolve({ status: 200 });
-    } else if (link === "noexiste.com") {
+    } else if (link === "linkerror.com") {
       return Promise.reject({});
+    } else if (link === "https://responseerror.com/") {
+      return Promise.reject({
+        response: {
+          status: 400,
+        },
+      });
+    } else if (link === "https://requesterror.com/") {
+      return Promise.reject({
+        request: {},
+      });
     }
   }),
 };
